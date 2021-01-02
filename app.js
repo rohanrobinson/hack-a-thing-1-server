03345d8cc35079d9a2f9b5411e7ee69d54db5ec5
Rohan Robinson <rohanrobinson24@gmail.com>
2021-01-01 20:46:04 -0600

    removed stuff, added json portion

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+    res.json({
+      message: "Greetings healthcare provider!!!",
+      patients: ["Patient 1", "Patient 2", "Patient 3", "Patient 4" ]
+    })
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +33,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message:err.message,
+    error: req.app.get('env') === 'development' ? err.stack : {}
+  });
 });
 
 module.exports = app;
