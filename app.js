23fd9ee0a09b4c4c0650247be8b8bc0bfa4295ba
Rohan Robinson <rohanrobinson24@gmail.com>
2021-01-02 20:46:15 -0600

    added cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 
 const app = express();
 
@@ -11,12 +11,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
       message: "Greetings healthcare provider!!!",
       patients: ["Patient 1", "Patient 2", "Patient 3", "Patient 4" ]
     })
+});
+
+app.get('/vitals', (req, res) => {
+  res.json({
+    vitals: ["vitals 1", "vitals 2", "vitals 3", "vitals 4" ]
+  })
 });
 
 
